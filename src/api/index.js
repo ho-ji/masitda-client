8f756c3f77d67ef9661c6cfc9aedba266c9828f8
ho-ji <wkdd0205@gmail.com>
2024-07-15 22:16:04 +0900

    refactor: interceptors 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,24 @@
 import axios from 'axios'
 
+let accessToken
+
+export const setAuthToken = (token) => {
+  accessToken = token
+}
+
 export const instance = axios.create({
   baseURL: 'https://acceptable-virgina-yeji-85400ab7.koyeb.app/api',
   withCredentials: true,
 })
-export const setAuthToken = (token) => {
-  instance.interceptors.request.use(
-    (config) => {
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-      }
-      return config
-    },
-    (error) => {
-      return Promise.reject(error)
+
+instance.interceptors.request.use(
+  (config) => {
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
     }
-  )
-}
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
